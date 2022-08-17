36aa86ef846968c1e409a27c0185448f1ca15958
Ben Stephens <65748213+BenStephens99@users.noreply.github.com>
2022-08-17 16:14:22 +0100

    commit

--- js/javascript.js
+++ js/javascript.js
@@ -12,7 +12,7 @@ function typewriter() {
   let currentHeader = 0;
 
   function writeLetter() {
-    if (currentHeader <= headers.length -1 ) {
+    if (currentHeader <= headers.length - 1) {
       if (currentLetter < (headerContent[currentHeader].length)) {
         headers[currentHeader].textContent += headerContent[currentHeader].charAt(currentLetter);
         currentLetter++;
@@ -34,7 +34,7 @@ var menuOpen = false;
 var siteNav = document.getElementById('siteNav');
 var img = document.getElementById('navMenuImage');
 
-function openMenu () {
+function openMenu() {
   if (!menuOpen) {
     console.log(siteNav);
     siteNav.style.display = 'block';
@@ -45,13 +45,35 @@ function openMenu () {
     img.style.transform = 'rotate(0deg)';
     menuOpen = false;
   }
-} 
+}
 
 window.addEventListener("mousemove", typewriter());
 
-function showMore (section){
+
+function showMore(section) {
   var targetDiv = document.getElementById(section);
   targetDiv.getElementsByClassName('readMoreButton')[0].style.display = 'none';
   targetDiv.getElementsByClassName('moreInfo')[0].style.display = 'block';
 
+}
+
+let currentSlide = 0;
+let slides = document.getElementsByClassName('slide');
+
+function slideshow () {
+
+  slides[currentSlide].style.display = 'block';
+  
+}
+slideshow();
+
+function plusSlides(n) {
+  document.getElementsByClassName('slide')[currentSlide].style.display = 'none';
+  currentSlide = currentSlide + n;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  
+  slideshow()
 }
